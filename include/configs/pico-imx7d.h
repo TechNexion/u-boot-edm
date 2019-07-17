41b78917996054e94ead3b7dae7a2eced6e6c37d
Po Cheng <po.cheng@technexion.com>
2019-07-17 16:29:21 +0800

    pico-imx7d: Modify uboot boot process to use boot.scr with distro configuration
    Signed-off-by: Po Cheng <po.cheng@technexion.com>

--- include/configs/pico-imx7d.h
+++ include/configs/pico-imx7d.h
@@ -16,8 +16,21 @@
 #include <linux/sizes.h>
 #include <asm/mach-imx/gpio.h>
 #include "mx7_common.h"
-#ifdef CONFIG_SPL
+
 #include "imx7_spl.h"
+
+/* SPL OS boot options - Falcon Mode */
+#ifdef CONFIG_SPL_OS_BOOT
+/* NOTE: we all work in sectors, i.e. a sector = 512 Bytes */
+#define CONFIG_SPL_FS_LOAD_ARGS_NAME	"fdtargs"
+#define CONFIG_SPL_FS_LOAD_KERNEL_NAME	"uImage"
+#define CONFIG_SYS_SPL_ARGS_ADDR	0x83000000 /* (CONFIG_SYS_SDRAM_BASE + (128 << 19)) i.e. 0x80000000 + 0x4000000 = 84000000 */
+#define CONFIG_CMD_SPL_WRITE_SIZE		(128 * SZ_1K)
+
+/* #define CONFIG_SYS_NAND_SPL_KERNEL_OFFS		0x500000  i.e. nand: sector 5242880, at 2,560 MB location */
+#define CONFIG_SYS_MMCSD_RAW_MODE_ARGS_SECTOR  2931712  /* CONFIG_ENV_OFFSET @ 64 * SZ_64K, CONFIG_ENV_SIZE = 0x1000 = 4K, 1st emmc partition starts at 16384 */
+#define CONFIG_SYS_MMCSD_RAW_MODE_ARGS_SECTORS 133      /* 133 * 512 = 68KB, e.g. (CONFIG_CMD_SPL_WRITE_SIZE / 512) */
+#define CONFIG_SYS_MMCSD_RAW_MODE_KERNEL_SECTOR        3000000  /* emmc: sector 3000000, at 1,536 MB location */
 #endif
 
 #ifdef CONFIG_SECURE_BOOT
@@ -34,10 +47,6 @@
 /* Size of malloc() pool */
 #define CONFIG_SYS_MALLOC_LEN		(32 * SZ_1M)
 
-/* #define CONFIG_MXC_GPIO -- redefined */
-
-#define CONFIG_MXC_UART
-#undef CONFIG_MXC_UART_BASE
 #define CONFIG_MXC_UART_BASE		UART5_IPS_BASE_ADDR
 
 /* allow to overwrite serial and ethaddr */
@@ -45,24 +54,20 @@
 #define CONFIG_CONS_INDEX		1
 #define CONFIG_BAUDRATE			115200
 
+/* FUSE command to burn fuse */
 #ifdef CONFIG_CMD_FUSE
 #define CONFIG_MXC_OCOTP
 #endif
 
-/* MMC Configs */
-#define CONFIG_FSL_ESDHC
-#define CONFIG_FSL_USDHC
-#define CONFIG_SYS_FSL_ESDHC_ADDR	0
-#define CONFIG_SUPPORT_EMMC_BOOT /* eMMC specific */
-
-/*  */
+/* Network */
 #define CONFIG_FEC_MXC
 #define CONFIG_MII
-#define CONFIG_FEC_XCV_TYPE             RGMII
-#define CONFIG_ETHPRIME                 "FEC"
+#define CONFIG_FEC_XCV_TYPE		RGMII
+#define CONFIG_ETHPRIME			"FEC"
 
 #define CONFIG_FEC_ENET_DEV 0
 
+/* ENET1 */
 #if (CONFIG_FEC_ENET_DEV == 0)
 #define IMX_FEC_BASE			ENET_IPS_BASE_ADDR
 #define CONFIG_FEC_MXC_PHYADDR          0x0
@@ -70,26 +75,13 @@
 #define IMX_FEC_BASE			ENET2_IPS_BASE_ADDR
 #define CONFIG_FEC_MXC_PHYADDR          0x1
 #endif
-
 #define CONFIG_FEC_MXC_MDIO_BASE	ENET_IPS_BASE_ADDR
 
-/* PMIC */
-#define CONFIG_POWER
-#define CONFIG_POWER_I2C
-#define CONFIG_POWER_PFUZE3000
-#define CONFIG_POWER_PFUZE3000_I2C_ADDR	0x08
-
-#undef CONFIG_BOOTM_NETBSD
-#undef CONFIG_BOOTM_PLAN9
-#undef CONFIG_BOOTM_RTEMS
-
-/* I2C configs */
-#define CONFIG_SYS_I2C
-#define CONFIG_SYS_I2C_MXC_I2C1         /* enable I2C bus 1 */
-#define CONFIG_SYS_I2C_MXC_I2C2         /* enable I2C bus 2 */
-#define CONFIG_SYS_I2C_MXC_I2C3         /* enable I2C bus 3 */
-#define CONFIG_SYS_I2C_MXC_I2C4		/* enable I2C bus 4 */
-#define CONFIG_SYS_I2C_SPEED		100000
+/* MMC Config */
+#define CONFIG_FSL_ESDHC
+#define CONFIG_FSL_USDHC
+#define CONFIG_SYS_FSL_ESDHC_ADDR	0
+#define CONFIG_SUPPORT_EMMC_BOOT /* eMMC specific */
 
 /* Command definition */
 
@@ -98,27 +90,13 @@
 
 #define CONFIG_SYS_AUXCORE_BOOTDATA 0x60000000 /* Set to QSPI1 A flash at default */
 
+#define SYS_AUXCORE_BOOTDATA_DDR	0x80000000
+#define SYS_AUXCORE_BOOTDATA_TCM	0x007f8000
+
 #ifdef CONFIG_IMX_BOOTAUX
 #define UPDATE_M4_ENV \
-	"tcm_addr=0x7F8000\0" \
 	"m4image=m4_tcm.bin\0" \
-	"loadm4image=fatload mmc ${mmcdev}:${mmcpart} ${tcm_addr} ${m4image}\0" \
-	"update_m4_from_sd=" \
-		"if sf probe 0:0; then " \
-			"if run loadm4image; then " \
-				"setexpr fw_sz ${filesize} + 0xffff; " \
-				"setexpr fw_sz ${fw_sz} / 0x10000; "	\
-				"setexpr fw_sz ${fw_sz} * 0x10000; "	\
-				"sf erase 0x0 ${fw_sz}; " \
-				"sf write ${loadaddr} 0x0 ${filesize}; " \
-			"fi; " \
-		"fi\0" \
-	"m4boot=" \
-		"if run loadm4image; then " \
-			"setenv mcu -m4; " \
-			"dcache flush; " \
-			"bootaux ${tcm_addr}; " \
-		"fi\0"
+	"m4_addr="__stringify(SYS_AUXCORE_BOOTDATA_TCM)"\0"
 #else
 #define UPDATE_M4_ENV ""
 #endif
@@ -130,186 +108,59 @@
 #define MFG_NAND_PARTITION ""
 #endif
 
-#ifdef CONFIG_VIDEO
-#define	CONFIG_VIDEO_MXS
-#define	CONFIG_VIDEO_LOGO
-#define	CONFIG_SPLASH_SCREEN
-#define CONFIG_SPLASH_SCREEN_ALIGN
-#define	CONFIG_CMD_BMP
-#define	CONFIG_BMP_16BPP
-#define	CONFIG_VIDEO_BMP_RLE8
-#define CONFIG_VIDEO_BMP_LOGO
-#define VIDEO_MODE "panel=MCIMX28LCD\0"
-#else
-#define VIDEO_MODE ""
-#endif
-
 #define CONFIG_MFG_ENV_SETTINGS \
 	"mfgtool_args=setenv bootargs console=${console},${baudrate} " \
 		"rdinit=/linuxrc " \
 		"g_mass_storage.stall=0 g_mass_storage.removable=1 " \
-		"g_mass_storage.idVendor=0x066F g_mass_storage.idProduct=0x37FF "\
-		"g_mass_storage.iSerialNumber=\"\" "\
-		MFG_NAND_PARTITION \
-		"clk_ignore_unused "\
-		"\0" \
+		"g_mass_storage.idVendor=0x066F g_mass_storage.idProduct=0x37FF " \
+		"g_mass_storage.iSerialNumber=\"\" " \
+		MFG_NAND_PARTITION "\0" \
+		"clk_ignore_unused\0" \
 	"initrd_addr=0x83800000\0" \
 	"initrd_high=0xffffffff\0" \
 	"bootcmd_mfg=run mfgtool_args;bootz ${loadaddr} ${initrd_addr} ${fdt_addr};\0" \
 
 #if defined(CONFIG_SYS_BOOT_NAND)
 #define CONFIG_EXTRA_ENV_SETTINGS \
-	CONFIG_MFG_ENV_SETTINGS \
-	VIDEO_MODE \
+	"console=ttymxc2\0" \
 	"fdt_addr=0x83000000\0" \
 	"fdt_high=0xffffffff\0"	  \
-	"console=ttymxc2\0" \
 	"bootargs=console=ttymxc2,115200 ubi.mtd=3 "  \
-		"root=ubi0:rootfs rootfstype=ubifs "		     \
-		"mtdparts=gpmi-nand:64m(boot),16m(kernel),16m(dtb),-(rootfs)\0"\
-	"bootcmd=nand read ${loadaddr} 0x4000000 0x800000;"\
-		"nand read ${fdt_addr} 0x5000000 0x100000;"\
+		"root=ubi0:rootfs rootfstype=ubifs " \
+		MFG_NAND_PARTITION "\0" \
+	"bootcmd=nand read ${loadaddr} 0x4000000 0x800000;" \
+		"nand read ${fdt_addr} 0x5000000 0x100000;" \
 		"bootz ${loadaddr} - ${fdt_addr}\0"
-
 #else
 #define CONFIG_EXTRA_ENV_SETTINGS \
-	CONFIG_MFG_ENV_SETTINGS \
 	UPDATE_M4_ENV \
-	VIDEO_MODE \
-	"script=boot.scr\0" \
-	"image=zImage\0" \
-	"stdout=serial\0" \
-	"console=ttymxc4\0" \
+	"console=ttymxc4,"__stringify(CONFIG_BAUDRATE)"\0" \
 	"splashpos=m,m\0" \
-	"som=imx7d\0" \
-	"baseboard=pi\0" \
-	"form=pico\0" \
-	"wifi_module=qca\0" \
-	"default_baseboard=pi\0" \
-	"fdt_high=0xffffffff\0" \
-	"initrd_high=0xffffffff\0" \
-	"fdt_addr=0x83000000\0" \
-	"boot_fdt=try\0" \
 	"ip_dyn=yes\0" \
-	"mmcdev="__stringify(CONFIG_SYS_MMC_ENV_DEV)"\0" \
-	"mmcpart=" __stringify(CONFIG_SYS_MMC_IMG_LOAD_PART) "\0" \
-	"searchbootdev=" \
-		"if test ${bootdev} = SD0; then " \
-			"setenv mmcrootdev /dev/mmcblk2; " \
-			"setenv mmcroot /dev/mmcblk2p2 rootwait rw; " \
-		"else " \
-			"setenv mmcrootdev /dev/mmcblk0; " \
-			"setenv mmcroot /dev/mmcblk0p2 rootwait rw; " \
-		"fi\0" \
-	"mmcautodetect=yes\0" \
-	"mmcargs=setenv bootargs console=${console},${baudrate} " \
-		"root=${mmcroot} \0" \
-	"loadbootscript=" \
-		"fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${script};\0" \
-	"bootscript=echo Running bootscript from mmc ...; " \
-		"source\0" \
-	"loadimage=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${image}\0" \
-	"setfdt=" \
-		"if test -n ${wifi_module} && test ${wifi_module} = qca; then " \
-			"setenv fdtfile ${som}-${form}-${wifi_module}_${baseboard}${mcu}.dtb; " \
-			"setenv form ${form}-${wifi_module}; " \
-		"else " \
-			"setenv fdtfile ${som}-${form}_${baseboard}${mcu}.dtb;" \
-		"fi\0" \
-	"loadfdt=fatload mmc ${mmcdev}:${mmcpart} ${fdt_addr} ${fdtfile}\0" \
-	"mmcboot=echo Booting from mmc ...; " \
-		"run m4boot; " \
-		"run searchbootdev; " \
-		"run mmcargs; " \
-		"echo baseboard is ${baseboard}; " \
-		"run setfdt; " \
-		"if test ${boot_fdt} = yes || test ${boot_fdt} = try; then " \
-			"if run loadfdt; then " \
-				"bootz ${loadaddr} - ${fdt_addr}; " \
-			"else " \
-				"if test ${boot_fdt} = try; then " \
-					"echo WARN: Cannot load the DT; " \
-					"echo fall back to load the default DT; " \
-					"setenv baseboard ${default_baseboard}; " \
-					"run setfdt; " \
-					"run loadfdt; " \
-					"bootz ${loadaddr} - ${fdt_addr}; " \
-				"else " \
-					"echo WARN: Cannot load the DT; " \
-				"fi; " \
-			"fi; " \
-		"else " \
-			"bootz; " \
-		"fi;\0" \
+	"fdtfile=undefined\0" \
+	"fdt_high=0xffffffff\0" \
+	"fdt_addr=0x83000000\0" \
+	"fdt_addr_R=0x83000000\0" \
+	"ramdisk_addr_r=0x83000000\0" \
+	"kernel_addr_r=" __stringify(CONFIG_LOADADDR) "\0" \
+	"pxefile_addr_r=" __stringify(CONFIG_LOADADDR) "\0" \
+	"scriptaddr=" __stringify(CONFIG_LOADADDR) "\0" \
+	"initrd_high=0xffffffff\0" \
+	"initrd_addr=0x83800000\0" \
 	"bootenv=uEnv.txt\0" \
-	"loadbootenv=fatload mmc ${mmcdev} ${loadaddr} ${bootenv}\0" \
-	"importbootenv=echo Importing environment from mmc ...; " \
-		"env import -t -r $loadaddr $filesize\0" \
-	"netargs=setenv bootargs console=${console},${baudrate} " \
-		"root=/dev/nfs " \
-		"ip=dhcp nfsroot=${serverip}:${nfsroot},v3,tcp\0" \
-	"netboot=echo Booting from net ...; " \
-		"if test ${ip_dyn} = yes; then " \
-			"setenv get_cmd dhcp; " \
-		"else " \
-			"setenv get_cmd tftp; " \
-		"fi; " \
-		"run loadbootenv; " \
-		"run importbootenv; " \
-		"run setfdt; " \
-		"run netargs; " \
-		"${get_cmd} ${loadaddr} ${image}; " \
-		"if test ${boot_fdt} = yes || test ${boot_fdt} = try; then " \
-			"if ${get_cmd} ${fdt_addr} ${fdtfile}; then " \
-				"bootz ${loadaddr} - ${fdt_addr}; " \
-			"else " \
-				"if test ${boot_fdt} = try; then " \
-					"bootz; " \
-				"else " \
-					"echo WARN: Cannot load the DT; " \
-				"fi; " \
-			"fi; " \
-		"else " \
-			"bootz; " \
-		"fi;\0" \
 	"fit_addr=0x87880000\0" \
-	"fit_high=0xffffffff\0" \
-	"loadfit=fatload mmc ${mmcdev}:${mmcpart} ${fit_addr} tnrescue.itb\0" \
-	"fitargs=setenv bootargs console=${console},${baudrate} root=/dev/ram0 rootwait rw " \
-		"modules-load=g_acm_ms g_acm_ms.stall=0 g_acm_ms.removable=1 g_acm_ms.file=${mmcrootdev} " \
-		"g_acm_ms.iSerialNumber=${ethaddr} g_acm_ms.iManufacturer=TechNexion\0" \
-	"fitboot=echo Booting from FIT image...; " \
-		"run searchbootdev; run setfdt; run fitargs; " \
-		"bootm ${fit_addr}#config@${som}-${form}_${baseboard};\0"
+	BOOTENV
 
-#define CONFIG_BOOTCOMMAND \
-	   "mmc dev ${mmcdev}; if mmc rescan; then " \
-		   "if run loadbootenv; then " \
-			   "echo Loaded environment from ${bootenv};" \
-			   "run importbootenv;" \
-		   "fi;" \
-		   "if test -n $uenvcmd; then " \
-			   "echo Running uenvcmd ...;" \
-			   "run uenvcmd;" \
-		   "fi;" \
-		   "if run loadbootscript; then " \
-			   "run bootscript; " \
-		   "fi; " \
-		   "if run loadfit; then " \
-			   "run fitboot; " \
-		   "fi; " \
-		   "if run loadimage; then " \
-			   "run mmcboot; " \
-		   "else " \
-			   "echo WARN: Cannot load kernel from boot media; " \
-		   "fi; " \
-	   "else run netboot; fi"
+#define BOOT_TARGET_DEVICES(func) \
+	func(MMC, mmc, 0) \
+	func(MMC, mmc, 1) \
+	func(DHCP, dhcp, na)
+
+#include <config_distro_bootcmd.h>
 #endif
 
 /* Miscellaneous configurable options */
-/* #define CONFIG_SYS_LONGHELP -- redefined */
 #define CONFIG_SYS_PROMPT		"=> "
-/* #define CONFIG_AUTO_COMPLETE -- redefined */
 
 /* Print Buffer Size */
 #define CONFIG_SYS_PBSIZE (CONFIG_SYS_CBSIZE + sizeof(CONFIG_SYS_PROMPT) + 16)
@@ -320,9 +171,6 @@
 
 #define CONFIG_SYS_LOAD_ADDR		CONFIG_LOADADDR
 #define CONFIG_SYS_HZ			1000
-
-/* #define CONFIG_CMDLINE_EDITING -- redefined */
-/* #define CONFIG_STACKSIZE		SZ_128K -- unused */
 
 /* Physical Memory Map */
 #define CONFIG_NR_DRAM_BANKS		1
@@ -337,7 +185,32 @@
 #define CONFIG_SYS_INIT_SP_ADDR \
 	(CONFIG_SYS_INIT_RAM_ADDR + CONFIG_SYS_INIT_SP_OFFSET)
 
-#define CONFIG_ENV_SIZE			SZ_8K
+/* I2C configs */
+#define CONFIG_SYS_I2C
+#define CONFIG_SYS_I2C_MXC_I2C1
+#define CONFIG_SYS_I2C_MXC_I2C2
+#define CONFIG_SYS_I2C_MXC_I2C3
+#define CONFIG_SYS_I2C_MXC_I2C4
+#define CONFIG_SYS_I2C_SPEED		100000
+
+#define CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG
+
+/* PMIC */
+#define CONFIG_POWER
+#define CONFIG_POWER_I2C
+#define CONFIG_POWER_PFUZE3000
+#define CONFIG_POWER_PFUZE3000_I2C_ADDR	0x08
+
+#ifdef CONFIG_VIDEO
+#define CONFIG_VIDEO_MXS
+#define CONFIG_VIDEO_LOGO
+#define CONFIG_SPLASH_SCREEN
+#define CONFIG_SPLASH_SCREEN_ALIGN
+#define CONFIG_CMD_BMP
+#define CONFIG_BMP_16BPP
+#define CONFIG_VIDEO_BMP_RLE8
+#define CONFIG_VIDEO_BMP_LOGO
+#endif
 
 #ifdef CONFIG_SYS_BOOT_QSPI
 #define CONFIG_SYS_USE_QSPI
@@ -345,15 +218,13 @@
 #elif defined CONFIG_SYS_BOOT_NAND
 #define CONFIG_SYS_USE_NAND
 #define CONFIG_ENV_IS_IN_NAND
-/* #else
-#define CONFIG_ENV_IS_IN_MMC -- redefined */
 #endif
 
+/* NAND stuff */
 #ifdef CONFIG_SYS_USE_NAND
 #define CONFIG_CMD_NAND
 #define CONFIG_CMD_NAND_TRIMFFS
 
-/* NAND stuff */
 #define CONFIG_NAND_MXS
 #define CONFIG_SYS_MAX_NAND_DEVICE	1
 #define CONFIG_SYS_NAND_BASE		0x40000000
@@ -382,16 +253,18 @@
 #endif
 
 #if defined(CONFIG_ENV_IS_IN_MMC)
-#define CONFIG_ENV_OFFSET		(8 * SZ_64K)
+#define CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG
+#define CONFIG_ENV_OFFSET		(8 * SZ_64K) /* at 512KB (sector 1024) */
+#define CONFIG_ENV_SIZE 		0x3000 /* 12,288 bytes (24 sectors) */
 #elif defined(CONFIG_ENV_IS_IN_SPI_FLASH)
 #define CONFIG_ENV_OFFSET		(768 * 1024)
 #define CONFIG_ENV_SECT_SIZE		(64 * 1024)
+#define CONFIG_ENV_SIZE 		CONFIG_ENV_SECT_SIZE
 #define CONFIG_ENV_SPI_BUS		CONFIG_SF_DEFAULT_BUS
 #define CONFIG_ENV_SPI_CS		CONFIG_SF_DEFAULT_CS
 #define CONFIG_ENV_SPI_MODE		CONFIG_SF_DEFAULT_MODE
 #define CONFIG_ENV_SPI_MAX_HZ		CONFIG_SF_DEFAULT_SPEED
 #elif defined(CONFIG_ENV_IS_IN_NAND)
-#undef CONFIG_ENV_SIZE
 #define CONFIG_ENV_OFFSET		(3 << 18)
 #define CONFIG_ENV_SECT_SIZE		(128 << 10)
 #define CONFIG_ENV_SIZE			CONFIG_ENV_SECT_SIZE
@@ -402,19 +275,14 @@
 #else
 #define CONFIG_SYS_FSL_USDHC_NUM	2
 #endif
-#define CONFIG_SYS_MMC_ENV_DEV		0   /* USDHC1 */
-#define CONFIG_SYS_MMC_ENV_PART		0	/* user area */
-#define CONFIG_MMCROOT			"/dev/mmcblk2p2"  /* USDHC1 */
+#define CONFIG_SYS_MMC_ENV_DEV		0			/* USDHC1 */
+#define CONFIG_SYS_MMC_ENV_PART		0			/* user area */
+#define CONFIG_MMCROOT			"/dev/mmcblk2p2"	/* USDHC1 */
 
 /*
  * FDT options
  */
 #define CONFIG_LMB
-
-/* #define CONFIG_CMD_BMODE -- redefined */
-
-/* #define CONFIG_SPLASH_SCREEN*/
-/* #define CONFIG_MXC_EPDC*/
 
 /*
  * SPLASH SCREEN Configs
@@ -440,9 +308,9 @@
 #define CONFIG_EHCI_HCD_INIT_AFTER_RESET
 #define CONFIG_USB_HOST_ETHER
 #define CONFIG_USB_ETHER_ASIX
-#define CONFIG_MXC_USB_PORTSC  (PORT_PTS_UTMI | PORT_PTS_PTW)
-#define CONFIG_MXC_USB_FLAGS   0
-#define CONFIG_USB_MAX_CONTROLLER_COUNT 2
+#define CONFIG_MXC_USB_PORTSC			(PORT_PTS_UTMI | PORT_PTS_PTW)
+#define CONFIG_MXC_USB_FLAGS			0
+#define CONFIG_USB_MAX_CONTROLLER_COUNT	2
 
 #if defined(CONFIG_MXC_EPDC) && defined(CONFIG_SYS_USE_QSPI)
 #error "EPDC Pins conflicts QSPI, Either EPDC or QSPI can be enabled!"
@@ -454,25 +322,4 @@
 #include "pico-imx7d_android.h"
 #endif
 
-/* SPL OS boot options - Falcon Mode */
-#ifdef CONFIG_SPL_OS_BOOT
-#undef CONFIG_SPL_FS_LOAD_ARGS_NAME
-#undef CONFIG_SPL_FS_LOAD_KERNEL_NAME
-#undef CONFIG_SYS_SPL_ARGS_ADDR
-#undef CONFIG_CMD_SPL_WRITE_SIZE
-#undef CONFIG_SYS_MMCSD_RAW_MODE_KERNEL_SECTOR
-#undef CONFIG_SYS_MMCSD_RAW_MODE_ARGS_SECTOR
-#undef CONFIG_SYS_MMCSD_RAW_MODE_ARGS_SECTORS
-
-/* NOTE: we all work in sectors, i.e. a sector = 512 Bytes */
-#define CONFIG_SPL_FS_LOAD_ARGS_NAME		"fdtargs"
-#define CONFIG_SPL_FS_LOAD_KERNEL_NAME		"uImage"
-#define CONFIG_SYS_SPL_ARGS_ADDR		0x83000000 /* (CONFIG_SYS_SDRAM_BASE + (128 << 19)) i.e. 0x80000000 + 0x4000000 = 84000000 */
-#define CONFIG_CMD_SPL_WRITE_SIZE		(128 * SZ_1K)
-
-/* #define CONFIG_SYS_NAND_SPL_KERNEL_OFFS		0x500000  i.e. nand: sector 5242880, at 2,560 MB location */
-#define CONFIG_SYS_MMCSD_RAW_MODE_KERNEL_SECTOR	3000000  /* emmc: sector 3000000, at 1,536 MB location */
-#define CONFIG_SYS_MMCSD_RAW_MODE_ARGS_SECTOR	2931712  /* CONFIG_ENV_OFFSET @ 64 * SZ_64K, CONFIG_ENV_SIZE = 0x1000 = 4K, 1st emmc partition starts at 16384 */
-#define CONFIG_SYS_MMCSD_RAW_MODE_ARGS_SECTORS	133      /* 133 * 512 = 68KB */
-#endif
 #endif	/* __PICO_IMX7D_CONFIG_H */
